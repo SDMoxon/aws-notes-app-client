1a5eab7c7109b36e6478eb1170e315466fce7b4d
Steven <steven_moxon123@hotmail.co.uk>
2018-02-18 20:52:11 +0000

    add s3 functinallity

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import LoaderButton from '../components/LoaderButton';
-import { invokeApig } from '../libs/awsLib';
+import { invokeApig, s3Upload } from '../libs/awsLib';
 import config from '../config';
 import './NewNote.css';
 
@@ -35,15 +35,20 @@ export default class NewNote extends Component {
     event.preventDefault();
   
     if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
-      alert('Please pick a file smaller than 5MB');
+      alert("Please pick a file smaller than 5MB");
       return;
     }
   
     this.setState({ isLoading: true });
   
     try {
+      const uploadedFilename = this.file
+        ? (await s3Upload(this.file)).Location
+        : null;
+  
       await this.createNote({
-        content: this.state.content
+        content: this.state.content,
+        attachment: uploadedFilename
       });
       this.props.history.push('/');
     } catch (e) {
