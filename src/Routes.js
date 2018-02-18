1aea1b4fa7747c71a2fdc0776bbdcc107a02c211
Steven <steven_moxon123@hotmail.co.uk>
2018-02-18 21:05:37 +0000

    add Note component

--- src/Routes.js
+++ src/Routes.js
@@ -6,12 +6,14 @@ import Login from './containers/Login';
 import AppliedRoute from './components/AppliedRoute';
 import NewNote from './containers/NewNote';
 import Signup from './containers/Signup';
+import Notes from './containers/Notes';
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path='/' exact component={Home} props={childProps} />
     <AppliedRoute path='/notes/new' exact component={NewNote} props={childProps} />
     <AppliedRoute path='/login' exact component={Login} props={childProps} />
+    <AppliedRoute path='/notes/:id' exact component={Notes} props={childProps} />
     <AppliedRoute path='/signup' exact component={Signup} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
